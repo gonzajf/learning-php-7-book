e1a907d33698a1be781ffdf9ee3e2371867fb673
Gonzalo <gonzalo.franchino@gmail.com>
2020-05-26 14:07:50 -0300

    reading json file and converting it to php array

--- basic-web/index.php
+++ basic-web/index.php
@@ -11,29 +11,8 @@ $looking = isset($_GET['title']) || isset($_GET['author']);
 
 <body>
 <?php
-$books = [
-    [
-        'title' => 'To Kill A Mockingbird',
-        'author' => 'Harper Lee',
-        'available' => true,
-        'pages' => 336,
-        'isbn' => 9780061120084
-    ],
-    [
-        'title' => '1984',
-        'author' => 'George Orwell',
-        'available' => true,
-        'pages' => 267,
-        'isbn' => 9780547249643
-    ],
-    [
-        'title' => 'One Hundred Years Of Solitude',
-        'author' => 'Gabriel Garcia Marquez',
-        'available' => false,
-        'pages' => 457,
-        'isbn' => 9785267006323
-    ],
-];
+$booksJson = file_get_contents('books.json');
+$books = json_decode($booksJson, true);
 ?>
 <?php foreach ($books as $book): ?>
 <ul>
