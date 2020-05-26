435daf8622d80aead9f5ec40df1dcfbb91e43eb5
Gonzalo <gonzalo.franchino@gmail.com>
2020-05-26 18:39:14 -0300

    autoload. small fix in book parameters

--- oop/init.php
+++ oop/init.php
@@ -3,12 +3,21 @@
 use Bookstore\Domain\Book;
 use Bookstore\Domain\Customer;
 
-require_once __DIR__ . '/Book.php';
-require_once __DIR__ . '/Customer.php';
+function __autoload($classname) {
+    $lastSlash = strpos($classname, '\\') + 1;
+    $classname = substr($classname, $lastSlash);
+    $directory = str_replace('\\', '/', $classname);
+    $filename = __DIR__ . '/src/' . $directory . '.php';
+    require_once($filename);
+}
 
-$book1 = new Book("1984", "George Orwell", 9785267006323, 12);
-$book2 = new Book("To Kill a Mockingbird", "Harper Lee", 9780061120084, 2);
+$book1 = new Book(9785267006323,"1984", "George Orwell",  12);
+$book2 = new Book(9780061120084, "To Kill a Mockingbird", "Harper Lee" , 2);
 
 $customer1 = new Customer(1, 'John', 'Doe', 'johndoe@mail.com');
 $customer2 = new Customer(2, 'Mary', 'Poppins', 'mp@mail.com');
 
+var_dump($book1);
+var_dump($book2);
+var_dump($customer1);
+var_dump($customer2);
