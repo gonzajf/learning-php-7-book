82350afe78c25b6df064b4f7f4832fb86c430b09
Gonzalo <gonzalo.franchino@gmail.com>
2020-05-26 11:39:43 -0300

    using loops

--- basic-web/index.php
+++ basic-web/index.php
@@ -9,6 +9,42 @@ $looking = isset($_GET['title']) || isset($_GET['author']);
 </head>
 
 <body>
+<?php
+$books = [
+    [
+        'title' => 'To Kill A Mockingbird',
+        'author' => 'Harper Lee',
+        'available' => true,
+        'pages' => 336,
+        'isbn' => 9780061120084
+    ],
+    [
+        'title' => '1984',
+        'author' => 'George Orwell',
+        'available' => true,
+        'pages' => 267,
+        'isbn' => 9780547249643
+    ],
+    [
+        'title' => 'One Hundred Years Of Solitude',
+        'author' => 'Gabriel Garcia Marquez',
+        'available' => false,
+        'pages' => 457,
+        'isbn' => 9785267006323
+    ],
+];
+?>
+<?php foreach ($books as $book): ?>
+<ul>
+    <li>
+        <i><?php echo $book['title']; ?></i> - <?php echo $book['author']; ?>
+        <?php if (!$book['available']): ?>
+            <b>Not available</b>
+        <?php endif; ?>
+    </li>
+</ul>
+<?php endforeach; ?>
+
     <p>
 <?php
     if (isset($_COOKIE['username'])) {
