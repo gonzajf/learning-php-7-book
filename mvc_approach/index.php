c8f15b0e3736718fc3d6229158374c97fc0f078e
Gonzalo <gonzalo.franchino@gmail.com>
2020-06-08 13:47:10 -0300

    book view

--- mvc_approach/index.php
+++ mvc_approach/index.php
@@ -1,3 +1,15 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
+
+use Bookstore\Core\Db;
+use Bookstore\Models\BookModel;
+
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/views');
+$twig = new Twig_Environment($loader);
+
+$bookModel = new BookModel(Db::getInstance());
+$book = $bookModel->get(1);
+
+$params = ['book' => $book];
+echo $twig->loadTemplate('book.twig')->render($params);
