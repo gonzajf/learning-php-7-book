9e058c8b067ccbc46338ea9e843b4c1e177f6221
Gonzalo <gonzalo.franchino@gmail.com>
2020-06-21 20:00:28 -0300

    refactoring trait. Generating unique id is not more needed.

--- mvc_approach/src/Utils/Unique.php
+++ mvc_approach/src/Utils/Unique.php
@@ -6,27 +6,10 @@ use Bookstore\Exceptions\InvalidIdException;
 
 trait Unique {
 
-    private static $lastId = 0;
     protected $id;
 
-    public function setId(int $id = null){
-
-        if($id < 0) {
-            throw new InvalidIdException("Id cannot be null");
-        }
-
-        if (empty($id)) {
-            $this->id = ++self::$lastId;
-        } else {
-            $this->id = $id;
-            if ($id > self::$lastId) {
-                self::$lastId = $id;
-            }
-        }
-    }
-
-    public static function getLastId(): int {
-        return self::$lastId;
+    public function setId($id) {
+        $this->id = $id;
     }
 
     public function getId(): int {
