dceea84fb45a05bc41356c933c0d1ca1d1c97cb4
Gonzalo <gonzalo.franchino@gmail.com>
2020-06-22 14:44:51 -0300

    setUp method for tests

--- mvc_approach/tests/Domain/Customer/BasicTest.php
+++ mvc_approach/tests/Domain/Customer/BasicTest.php
@@ -7,15 +7,20 @@ use PHPUnit_Framework_TestCase;
 
 class BasicTest extends PHPUnit_Framework_TestCase {
 
-    public function testAmountToBorrow() {
+    private $customer;
 
-        $customer = new Basic(1, 'han', 'solo', 'han@solo.com');
-
-        $this->assertSame(
-            3,
-            $customer->getAmountToBorrow(),
-            'Basic customer should borrow up to 3 books.'
+    public function setUp() {
+        $this->customer = new Basic(
+            1, 'han', 'solo', 'han@solo.com'
         );
     }
 
+    public function testAmountToBorrow() {
+
+        $this->assertSame(
+            3,
+            $this->customer->getAmountToBorrow(),
+            'Basic customer should borrow up to 3 books.'
+        );
+    }
 }
